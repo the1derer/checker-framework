60175c5c965b4bfbb5b3d82e26560740daa2c4ab
Shubham Raj <shubhamraj1in@gmail.com>
2019-06-21 22:31:49 +0530

    Correct Javadoc
    Signed-off-by: Shubham Raj <shubhamraj1in@gmail.com>

--- javacutil/src/main/java/org/checkerframework/javacutil/PluginUtil.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/PluginUtil.java
@@ -37,7 +37,7 @@ public class PluginUtil {
      * Option name for specifying an alternative jdk.jar location in case of Java8. The accompanying
      * value MUST be the path to the jar file (NOT the path to its encompassing directory) In Case
      * of Java 9+ this option specifies directory containing alternate <modules>.jar which will be
-     * patched using {@code --patch-module <module>=<NewModule>}
+     * patched using {@code --patch-module &lt;module&gt;=&lt;NewModule&gt;}
      */
     public static final String JDK_PATH_OPT = "-jdkJar";
 
